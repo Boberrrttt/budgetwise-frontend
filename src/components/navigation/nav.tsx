c071f071b96c4874f9ee37ea02c7dd9da66a76e5
Bobert <salvo.robertroy@gmail.com>
2025-01-22 16:09:20 +0800

    refactor: added color to navbar

--- src/components/navigation/nav.tsx
+++ src/components/navigation/nav.tsx
@@ -7,10 +7,10 @@ interface NavProps {
 
 const Nav = ( { groupname }: NavProps) => {
     return (
-        <nav className="border-b-2 border-black dark:border-white p-3 flex justify-between">
-            <Link href={'/home'} className="font-bold text-brandPrimary dark:text-brandPrimary text-4xl">BudgetWise</Link>
+        <nav className="border-b-2 bg-brandPrimary shadow-lg p-3 flex py-4 justify-between">
+            <Link href={'/home'} className="font-bold text-white  text-4xl">BudgetWise</Link>
             <div className="flex gap-5">
-                <h1 className="text-4xl text-brandPrimary font-bold">{groupname}</h1>
+                <h1 className="text-4xl text-white font-bold">{groupname}</h1>
                 <ToggleThemeButton isAuthenticated={true}/>
             </div>
         </nav>
